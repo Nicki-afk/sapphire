2e10c37963cc94ade9d94c0515574528a331ace3
Nicki-afk <casperplay45@gmail.com>
2023-07-26 15:53:59 +0300

    Дописан предпусковой скрипт

--- pre-start.sh
+++ pre-start.sh
@@ -1,4 +1,5 @@
 pom_file="pom.xml"   # The file name 
+line_to_change=84    # The string var
 
 # Check for file exisist
 # Проверка существует ли файл 
@@ -6,3 +7,32 @@ if [ ! -f "$file" ]; then
   echo "File not found or it's don't simple file : $pom_file"
   exit 1
 fi
+
+# Check for right
+# Проверка на права для файла 
+if [! -r "$pom_file"] || [! -w "$pom_file"]; then 
+        echo "File cannot be written or read (permission denied) try running with sudo"
+        exit 1 
+fi    
+
+
+# Requesting your path
+# Запрос пути каталога в котором находится tomacat 
+
+echo "### ATTENTION !! Specify the path to the tomcat directory before the webapps directories ( '/webapps/' ) ###"
+read -p "Write directory Tomcat in which you want install application : " tomacat_directory
+
+
+
+# Request confirmation
+# Запрашиваем потверждение 
+read -p "Are you sure you want to replace the $line_to_change string with \"$tomacat_directory\"? (y/n):" confirm
+if [ "$confirm" != "y" ]; then
+  echo "Operation cancaled."
+  exit 0
+fi
+
+
+sed -i "${line_to_change}s#.*#$tomacat_directory#" "$pom_file"
+
+echo "String $line_number in file change successful to :  \"$tomacat_directory\"."
