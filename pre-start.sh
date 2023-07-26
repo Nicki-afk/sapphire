8bd9f7178074f342b01429ee23a10e9def985baa
Nicki-afk <casperplay45@gmail.com>
2023-07-26 16:16:24 +0300

    Добавлены правки по предстартовому скрипту

--- pre-start.sh
+++ pre-start.sh
@@ -20,19 +20,20 @@ fi
 # Запрос пути каталога в котором находится tomacat 
 
 echo "### ATTENTION !! Specify the path to the tomcat directory before the webapps directories ( '/webapps/' ) ###"
-read -p "Write directory Tomcat in which you want install application : " tomacat_directory
+read -p "Write directory Tomcat in which you want install application : " tomcat_directory
 
 
 
 # Request confirmation
 # Запрашиваем потверждение 
-read -p "Are you sure you want to replace the $line_to_change string with \"$tomacat_directory\"? (y/n):" confirm
+read -p "Are you sure you want to replace the $line_to_change string with \"$tomcat_directory\"? (y/n):" confirm
 if [ "$confirm" != "y" ]; then
   echo "Operation cancaled."
   exit 0
 fi
 
+formating_path="          <outputDirectory>$tomcat_directory</outputDirectory>"
 
-sed -i "${line_to_change}s#.*#$tomacat_directory#" "$pom_file"
+sed -i "${line_to_change}s#.*#$formating_path#" "$pom_file"
 
 echo "String $line_number in file change successful to :  \"$tomacat_directory\"."
