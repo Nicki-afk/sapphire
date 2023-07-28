28e2bae7da2a9584ad7e1feea66435b9ca139fff
Nicki-afk <casperplay45@gmail.com>
2023-07-28 23:18:34 +0300

    Обновлен объект Message

--- src/main/java/gyber/websocket/messageConfig/Message.java
+++ src/main/java/gyber/websocket/messageConfig/Message.java
@@ -2,7 +2,7 @@ package gyber.websocket.messageConfig;
 
 public class Message {
     private String from;
-    private String to;
+    private String prefixTo;
     private String content;
 
 
@@ -12,9 +12,9 @@ public class Message {
 
 
 
-    public Message(String from, String to, String content) {
+    public Message(String from, String prefixTo, String content) {
         this.from = from;
-        this.to = to;
+        this.prefixTo = prefixTo;
         this.content = content;
     }
 
@@ -25,11 +25,11 @@ public class Message {
     public void setFrom(String from) {
         this.from = from;
     }
-    public String getTo() {
-        return to;
+    public String getPrefixTo() {
+        return prefixTo;
     }
-    public void setTo(String to) {
-        this.to = to;
+    public void setPrefixTo(String prefixTo) {
+        this.prefixTo = prefixTo;
     }
     public String getContent() {
         return content;
