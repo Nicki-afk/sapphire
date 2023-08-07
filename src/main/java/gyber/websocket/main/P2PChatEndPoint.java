a7a3919c18238c4c5f286f0328ffda7c268574d3
Nicki-afk <casperplay45@gmail.com>
2023-08-07 13:37:41 +0300

    Удален лишний функционал. Концтрация на первостепенной пересылки сообщений пользователям

--- src/main/java/gyber/websocket/main/P2PChatEndPoint.java
+++ src/main/java/gyber/websocket/main/P2PChatEndPoint.java
@@ -20,13 +20,13 @@ import gyber.websocket.messageConfig.MessageEncoder;
 
 @ServerEndpoint(value = "/chatfor/" , encoders = MessageEncoder.class , decoders = MessageDecoder.class)
 public class P2PChatEndPoint  {
-    private Chat chat;
+  //  private Chat chat;
     private Session session;
 
-    private List<Chat>chatsList = new ArrayList<Chat>();
+    // private List<Chat>chatsList = new ArrayList<Chat>();
     private Logger logger = Logger.getLogger("P2PChat");
-    private Map<String , Chat>sessionIdAndChat = new HashMap<>();
-    private AtomicInteger atomicInteger = new AtomicInteger(0);
+    // private Map<String , Chat>sessionIdAndChat = new HashMap<>();
+   //  private AtomicInteger atomicInteger = new AtomicInteger(0);
     private static final Set<P2PChatEndPoint> clients = new CopyOnWriteArraySet<>();
 
 
@@ -35,36 +35,7 @@ public class P2PChatEndPoint  {
     public void onOpen(Session session){
         Map<String , List<String>>onOpenData = session.getRequestParameterMap();
 
-        String personOne = onOpenData.get("personOne").get(0);
-        String personTwo = onOpenData.get("personTwo").get(0);
-        String chatId = onOpenData.get("chatId").get(0);
-
-        session.setMaxIdleTimeout(120000);
-
-        this.session = session;
-        clients.add(this);
-
-
-        if(chatId == null){
-            this.chat = new Chat(atomicInteger.incrementAndGet() , personOne, personTwo);
-            this.chatsList.add(this.chat);
-            this.sessionIdAndChat.put(session.getId(), chat);
-
-        }else{
-
-            //this.chat = this.chatsList.get(this.chatsList.indexOf(new Chat(Integer.parseInt(chatId), personOne, personTwo)));
-
-            if(this.chatsList.contains(new Chat(Integer.parseInt(chatId) , personOne , personTwo))){
-                logger.info("OK chat exists");
-
-
-            }else{
-                logger.info("chat don't exist");
-            }
-
-
-        }
-
+       
 
     }
 
@@ -78,7 +49,12 @@ public class P2PChatEndPoint  {
 
 
 
-    public void send(){
+    public void send(Message message){
+
+       
+
+
+
 
     }
     
