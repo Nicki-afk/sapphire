6e67c5d8d4df2661ac14eca20de70983d1e87280
Nicki-afk <casperplay45@gmail.com>
2023-08-07 13:26:40 +0300

    Добавлен первичный функционал метода onOpen. Добавлен пустой метод отправки сооьщений

--- src/main/java/gyber/websocket/main/P2PChatEndPoint.java
+++ src/main/java/gyber/websocket/main/P2PChatEndPoint.java
@@ -1,16 +1,20 @@
 package gyber.websocket.main;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
 
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import gyber.websocket.messageConfig.Message;
 import gyber.websocket.messageConfig.MessageDecoder;
 import gyber.websocket.messageConfig.MessageEncoder;
 
@@ -20,6 +24,8 @@ public class P2PChatEndPoint  {
     private Session session;
 
     private List<Chat>chatsList = new ArrayList<Chat>();
+    private Logger logger = Logger.getLogger("P2PChat");
+    private Map<String , Chat>sessionIdAndChat = new HashMap<>();
     private AtomicInteger atomicInteger = new AtomicInteger(0);
     private static final Set<P2PChatEndPoint> clients = new CopyOnWriteArraySet<>();
 
@@ -34,22 +40,45 @@ public class P2PChatEndPoint  {
         String chatId = onOpenData.get("chatId").get(0);
 
         session.setMaxIdleTimeout(120000);
-        this.session = session;
 
+        this.session = session;
         clients.add(this);
 
 
         if(chatId == null){
             this.chat = new Chat(atomicInteger.incrementAndGet() , personOne, personTwo);
             this.chatsList.add(this.chat);
+            this.sessionIdAndChat.put(session.getId(), chat);
 
+        }else{
+
+            //this.chat = this.chatsList.get(this.chatsList.indexOf(new Chat(Integer.parseInt(chatId), personOne, personTwo)));
+
+            if(this.chatsList.contains(new Chat(Integer.parseInt(chatId) , personOne , personTwo))){
+                logger.info("OK chat exists");
+
+
+            }else{
+                logger.info("chat don't exist");
+            }
 
 
         }
 
 
-        
+    }
 
+
+    @OnMessage
+    public void onMessage(Session session , Message message){
+
+            
+    }
+
+
+
+
+    public void send(){
 
     }
     
