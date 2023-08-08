91f51ff5d8daccade378adc45b1feb177bbb114c
Nicki-afk <casperplay45@gmail.com>
2023-08-08 23:19:01 +0300

    Изменен функционал SecurityMessage

--- src/main/java/gyber/websocket/messageConfig/SecurityMessage.java
+++ src/main/java/gyber/websocket/messageConfig/SecurityMessage.java
@@ -1,28 +1,42 @@
 package gyber.websocket.messageConfig;
 
 import java.security.PublicKey;
+import java.util.Base64;
 
 public class SecurityMessage extends Message{
 
     
-    private PublicKey publicKey;
+    private String pubKey;
+
 
 
     public SecurityMessage(String from , String to , PublicKey publicKey){
         super(from, to, " ", "PUBLIC_KEY_EXCHANGE");
+
+        this.pubKey = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+
+    }
+
     
-        this.publicKey = publicKey;
+   
+
+
+    public String getPubKey() {
+        return pubKey;
     }
 
 
-    public PublicKey getPublicKey() {
-        return publicKey;
+    public void setPubKey(String pubKey) {
+        this.pubKey = pubKey;
     }
 
 
-    public void setPublicKey(PublicKey publicKey) {
-        this.publicKey = publicKey;
-    }
+  
+
+    
+
+
+    
     
     
 
