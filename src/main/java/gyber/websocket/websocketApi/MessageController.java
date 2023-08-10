9fac22d0a56af784e72b617bf737078850b5b24a
Nicki-afk <casperplay45@gmail.com>
2023-08-10 14:05:09 +0300

    Добавлена новая 'ручка' для обмена ключами между пользователями

--- src/main/java/gyber/websocket/websocketApi/MessageController.java
+++ src/main/java/gyber/websocket/websocketApi/MessageController.java
@@ -1,14 +1,9 @@
 package gyber.websocket.websocketApi;
 
-import java.text.SimpleDateFormat;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.util.HtmlUtils;
 
 import gyber.websocket.messageConfig.Message;
 
@@ -26,6 +21,14 @@ public class MessageController {
         // String time = new SimpleDateFormat("HH:mm").format(new Date());
         return message;
     }   
+
+
+    @MessageMapping("/chacngekey")
+    public Message sendTo(Message message){
+
+        return null;
+
+    }
 
     
 }
