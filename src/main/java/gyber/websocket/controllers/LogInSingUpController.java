2cf920c18e30b7d741069e0136b373a2df37870e
Nicki-afk <casperplay45@gmail.com>
2023-08-12 23:48:33 +0300

    Добавлен новый контроллер регистраций и авторизации

---
+++ src/main/java/gyber/websocket/controllers/LogInSingUpController.java
@@ -0,0 +1,9 @@
+package gyber.websocket.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LogInSingUpController {
+    
+}
