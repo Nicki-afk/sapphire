f970bc5a26c36970a3564e700ea3bd7f3a9a5de3
Nicki-afk <casperplay45@gmail.com>
2023-08-14 14:24:46 +0300

    Добавлена простая ручка для регистрации

--- src/main/java/gyber/websocket/controllers/LogInSingUpController.java
+++ src/main/java/gyber/websocket/controllers/LogInSingUpController.java
@@ -1,9 +1,23 @@
 package gyber.websocket.controllers;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+import gyber.websocket.models.User;
+
+@Controller("/user")
 public class LogInSingUpController {
+
+
+    @PostMapping("/reg")
+    public ResponseEntity registerUser(@RequestBody User user){
+
+        return ResponseEntity.ok().build();
+    }
     
 }
