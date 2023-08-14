51620f2b5373b9860883a7fb122ce2187a58fd97
Nicki-afk <casperplay45@gmail.com>
2023-08-14 14:44:39 +0300

    Добавлен DTO класс

---
+++ src/main/java/gyber/websocket/models/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package gyber.websocket.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDTO {
+    private Long id;
+
+    private String userName;
+    private String firstName;
+    private String lastName;
+    
+
+    
+}
