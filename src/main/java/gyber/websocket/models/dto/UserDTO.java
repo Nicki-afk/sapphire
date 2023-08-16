9dd4f0d9c4a41c29eaa881703fd029a013be1ecf
Nicki-afk <casperplay45@gmail.com>
2023-08-16 21:10:29 +0300

    Добавлено оформление сущности для БД

--- src/main/java/gyber/websocket/models/dto/UserDTO.java
+++ src/main/java/gyber/websocket/models/dto/UserDTO.java
@@ -1,6 +1,13 @@
 package gyber.websocket.models.dto;
 
 import gyber.websocket.models.NetStatus;
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,11 +15,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class UserDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String userName;
+
+    @Enumerated(EnumType.STRING)
     private NetStatus onlineStatus;
+    
     private String ipfsDataHash;
 
 
