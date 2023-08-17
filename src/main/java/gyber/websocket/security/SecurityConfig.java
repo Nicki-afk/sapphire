9c3be047fae95d8ec391d1c054fcaf64bc7e0f6d
Nicki-afk <casperplay45@gmail.com>
2023-08-17 12:30:51 +0300

    Добавлен класс отвечающий за безопасность

---
+++ src/main/java/gyber/websocket/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package gyber.websocket.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+    
+
+}
