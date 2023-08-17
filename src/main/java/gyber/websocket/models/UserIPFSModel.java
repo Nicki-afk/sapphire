562ffea5ef566c17aa7f290b01ab3ff4fe315552
Nicki-afk <casperplay45@gmail.com>
2023-08-17 17:21:07 +0300

    Добавлены свойства сущности

--- src/main/java/gyber/websocket/models/UserIPFSModel.java
+++ src/main/java/gyber/websocket/models/UserIPFSModel.java
@@ -2,10 +2,33 @@ package gyber.websocket.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class UserIPFSModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private NetStatus onliNetStatus;
-    private String hashUserFile;
+
+    @Column(name = "username")         private String userName;
+    @Column(name = "crypto_wallet")    private String cryptoWalletAddress;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "net_status")
+    private NetStatus onlineNetStatus;
+    
+    @Column(name = "hash_data") private String hashUserFile;
 
 
     @JsonIgnore
@@ -14,8 +37,9 @@ public class UserIPFSModel {
     public UserIPFSModel(User user){
         this.user = user;
         this.id = user.getId();
-        this.onliNetStatus = user.getOnlineStatus();
-        // IPFS logic
+        this.userName = user.getUserName();
+        this.cryptoWalletAddress = user.getCryptoWallet();
+        this.onlineNetStatus = user.getOnlineStatus();
 
     }
 
