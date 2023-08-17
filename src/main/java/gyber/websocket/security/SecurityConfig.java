14126d2fc252451e31eb0bd1cac980a10e9d3081
Nicki-afk <casperplay45@gmail.com>
2023-08-17 14:08:25 +0300

    Добавлен метод в SecurityConfig

--- src/main/java/gyber/websocket/security/SecurityConfig.java
+++ src/main/java/gyber/websocket/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package gyber.websocket.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,7 +12,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/pub/**").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .permitAll()
+        .and()
+        .logout();
+    }
 
+  
+
+ 
 
     
 
