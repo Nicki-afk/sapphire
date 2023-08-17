c0e1f6f22864ffae9e74dfd26b9508bf1e3ebe31
Nicki-afk <casperplay45@gmail.com>
2023-08-17 14:07:32 +0300

    Добавлен репозиторий для пользователя

---
+++ src/main/java/gyber/websocket/models/repo/UserRepository.java
@@ -0,0 +1,13 @@
+package gyber.websocket.models.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import gyber.websocket.models.User;
+import gyber.websocket.models.UserIPFSModel;
+
+public interface UserRepository extends JpaRepository<Integer , User>{
+
+    UserIPFSModel findById(Long id);
+    UserIPFSModel findByUsername(String userName);
+    
+}
