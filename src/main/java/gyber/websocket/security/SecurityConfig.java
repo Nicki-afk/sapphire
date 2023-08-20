512d9c2332f456c60c12d40e3e10751e5734c99b
Nicki-afk <casperplay45@gmail.com>
2023-08-20 21:44:16 +0300

    Добавлен новый метод по конфигурации UserDetailsSe

--- src/main/java/gyber/websocket/security/SecurityConfig.java
+++ src/main/java/gyber/websocket/security/SecurityConfig.java
@@ -1,16 +1,23 @@
 package gyber.websocket.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import gyber.websocket.models.UserIPFSCustomDetailsService;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserIPFSCustomDetailsService userIPFSCustomDetailsService;
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,16 +28,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .anyRequest().authenticated()
         .and()
         .formLogin()
-        .loginPage("/login")
         .permitAll()
         .and()
         .logout();
     }
 
-  
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userIPFSCustomDetailsService);
+    }
 
- 
-
-    
 
 }
