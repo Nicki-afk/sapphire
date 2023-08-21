537a50f749fc2be1d459581210a279ac535052d1
Nicki-afk <casperplay45@gmail.com>
2023-08-21 16:22:57 +0300

    Временно закоментирован класс конфигурации

--- src/main/java/gyber/websocket/security/SecurityConfig.java
+++ src/main/java/gyber/websocket/security/SecurityConfig.java
@@ -1,23 +1,24 @@
 package gyber.websocket.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 import gyber.websocket.models.UserIPFSCustomDetailsService;
 
 
 // @Configuration
 // @EnableWebSecurity
-// public class SecurityConfig  {
-
-//     @Autowired
-//     private UserIPFSCustomDetailsService userIPFSCustomDetailsService;
+// public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 
 
-    
+
 
 
 
