2d8da520d0d139a02d6c3fb40743fd54168c9b71
Nicki-afk <casperplay45@gmail.com>
2023-08-22 21:12:54 +0300

    Написаны базовые ручки дял тестов

--- src/main/java/gyber/websocket/controllers/LogInSingUpController.java
+++ src/main/java/gyber/websocket/controllers/LogInSingUpController.java
@@ -3,39 +3,48 @@ package gyber.websocket.controllers;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 // import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import gyber.websocket.models.User;
 import gyber.websocket.models.UserIPFSModel;
+import gyber.websocket.models.repo.UserRepository;
 
 @Controller()
 @RequestMapping("/register")
 public class LogInSingUpController {
 
 
-    // @PostMapping
-    // public ResponseEntity postUserEntity(@ResponseBody Map<String , String> clientData){
-    //     String crypto = clientData.get("sign");
-
-
-
-
-    // }
+    @Autowired
+    private UserRepository userRepository;
 
 
     @PostMapping
     public ResponseEntity postUser(@RequestBody UserIPFSModel userIPFSModel){
 
+        this.userRepository.save(userIPFSModel);
 
 
         return userIPFSModel == null ? ResponseEntity.badRequest().build() : ResponseEntity.ok().build(); 
 
+
+    }
+
+
+    @GetMapping("/get")
+    public ResponseEntity getUser(@RequestParam("username") String username){
+
+        UserIPFSModel userIPFSModel = this.userRepository.findByUserName(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return ResponseEntity.ok(userIPFSModel);
 
     }
 }
