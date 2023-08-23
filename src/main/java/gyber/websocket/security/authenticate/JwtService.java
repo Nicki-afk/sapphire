21fbd9f421a5ccfb463c08859658e22c9aae5595
Nicki-afk <casperplay45@gmail.com>
2023-08-23 22:18:54 +0300

    Добавлен класс JwtService

---
+++ src/main/java/gyber/websocket/security/authenticate/JwtService.java
@@ -0,0 +1,58 @@
+package gyber.websocket.security.authenticate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import gyber.websocket.models.UserIPFSModel;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class JwtService implements TokenAuthenticate {
+
+
+   @Value("${jwt.token.validity.time}") private long validTime;
+   @Value("${sign.token.sign}")  private String singature;
+   
+
+
+
+
+
+
+
+    @Override
+    public String createToken(UserIPFSModel userIPFSModel) {
+       
+
+        LocalDateTime timeToSet = LocalDateTime.now().plusHours(4);
+        Date dateToSet = Date.from(timeToSet.atZone(ZoneId.systemDefault()).toInstant());
+
+        Map<String , Object>payload = new HashMap<>();
+        payload.put("userId", userIPFSModel.getId());
+        payload.put("username", userIPFSModel.getUserName());
+
+
+
+
+
+       return Jwts.
+             builder()
+            .setExpiration(dateToSet)
+            .signWith(SignatureAlgorithm.HS256 , singature)
+            .setSubject("JWT-DETAILS")
+            .setClaims(payload)
+            .setIssuedAt(new Date())
+            .compact();
+
+    }
+
+
+}
