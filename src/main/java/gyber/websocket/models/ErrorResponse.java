4cfabd9d17371d5bb0ca4e2f8043f7b2b401f15b
Nicki-afk <casperplay45@gmail.com>
2023-08-26 12:45:53 +0300

    Добавлен объект ошибки

---
+++ src/main/java/gyber/websocket/models/ErrorResponse.java
@@ -0,0 +1,38 @@
+package gyber.websocket.models;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorResponse {
+    
+    private String formatErrorTime;
+    private int errorCode;
+    private String  exceptionDescription;
+    private String message;
+
+
+ 
+    public ErrorResponse(){}
+
+    public ErrorResponse(LocalDateTime localDateTime , int errorCode , Exception exception , String message){
+        this.formatErrorTime = localDateTime.format(DateTimeFormatter.ofPattern("HH:mm:ss-dd:MM:yyyy"));
+        this.errorCode = errorCode;
+        this.exceptionDescription = exception.getMessage();
+        this.message = message;
+
+    }
+
+
+
+    
+}
