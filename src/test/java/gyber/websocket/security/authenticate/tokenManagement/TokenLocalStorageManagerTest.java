5a1c7debb9f29aa805d12f5da9aa28db4c7d4181
Nicki-afk <casperplay45@gmail.com>
2023-08-28 12:41:02 +0300

    Создан пустой тестовый класс

---
+++ src/test/java/gyber/websocket/security/authenticate/tokenManagement/TokenLocalStorageManagerTest.java
@@ -0,0 +1,43 @@
+package gyber.websocket.security.authenticate.tokenManagement;
+
+import org.junit.Test;
+
+public class TokenLocalStorageManagerTest {
+
+
+    
+    @Test
+    public void testAddTokenPairForUser() {
+
+    }
+
+    @Test
+    public void testExistTokenPair() {
+
+    }
+
+    @Test
+    public void testIsRefreshTokenBelongsThisUser() {
+
+    }
+
+    @Test
+    public void testIsRefreshTokenBelongsThisUser2() {
+
+    }
+
+    @Test
+    public void testJwtTokenIsValid() {
+
+    }
+
+    @Test
+    public void testRefreshTokenIsValid() {
+
+    }
+
+    @Test
+    public void testUpdateTokenPairUser() {
+
+    }
+}
