38923153b20737570dbcc57890731f29c64dcdd4
Nicki-afk <casperplay45@gmail.com>
2023-08-28 14:35:13 +0300

    Добавлена логика для установки свойств в файл

--- startup.sh
+++ startup.sh
@@ -14,7 +14,7 @@ echo "
  
 ---------------------------------------------------------------
 
-@scriptversion : 0.0.1
+@scriptversion : 0.0.2
 @author        : Nicki-afk ( https://github.com/Nicki-afk )
 @repository    : https://github.com/Nicki-afk/j-messanger-server/tree/main
 @releasedate   : 28:08:2023
@@ -34,6 +34,59 @@ startMvnw(){
     bash ./mvnw install spring-boot:run
 }
 
+writeAProperties(){
+
+  printLog "Start writing ... "
+  
+  echo "
+
+  spring.datasource.url=jdbc:h2:mem:testdb
+  spring.datasource.driver-class-name=org.h2.Driver
+  spring.datasource.username=sa
+  spring.datasource.password=
+  spring.jpa.database-platform=org.hibernate.dialect.H2Dialect
+  spring.jpa.show-sql=true
+  logging.level.org.hibernate.SQL=DEBUG
+
+  spring.h2.console.enabled=true
+  server.port=8081
+
+  # tokens settings
+  jwt.token.validity.time=14400000
+  jwt.token.sign=jszzGehyVyWZJmalACLgJEJqkykJYsVnipYXdJHtUCckimchXSjszzGehyVyWZJmalACLgJEJqkykJYsVnipYXdJHtUCckimchXS
+  rt.token.sign=fmvBydeuEYhSWiUoimEEwxKyrNGjgRzgyqjSgZRUVOaNcjmfJwfmvBydeuEYhSWiUoimEEwxKyrNGjgRzgyqjSgZRUVOaNcjmfJw
+    
+  
+  
+  " > "application.properties"
+
+  printLog "File application.properties write successful !!! "
+}
+
+
+configurePropertyFile(){
+
+  printLog "Start config property file for test ..."
+  cd src/main/resources
+
+  property_file="application.properties"
+
+  if [ ! -f "$property_file" ]; then
+    printLog "The property file don't exisist"
+    printLog "Creating property file ... "
+    touch "application.properties"
+    printLog "File creating successful !!! Writing a test properties..."
+    writeAProperties
+
+  else
+    printLog "File property exist"
+  fi  
+
+  
+
+
+}
+
 
 printLog "Check Java and Java version ..."
 
@@ -50,17 +103,20 @@ if [ -z "$java_version" ]; then
   sudo apt update
   sudo apt install -y openjdk-17-jdk
   printLog "Java install successful !!"
+  configurePropertyFile
   startMvnw
 else
   major_version=$(echo "$java_version" | cut -d'.' -f1)
   if [ "$major_version" -eq "17" ]; then
    printLog "Your java version is actual"
-   startMvnw
+   configurePropertyFile
+  startMvnw
   else
     printLog "Java is installed, but it is not version 17. It is version $java_version. Installing the java 17..."
     sudo apt update
     sudo apt install -y openjdk-17-jdk
     printLog "Java install successful !!"
+    configurePropertyFile
     startMvnw
 
   fi
