16d9d3f1d82f12bf045d5c6b65d19b8ebc826bbc
Nicki-afk <casperplay45@gmail.com>
2023-09-06 20:20:34 +0300

    Добавлен бин

--- src/main/java/gyber/MainClass.java
+++ src/main/java/gyber/MainClass.java
@@ -2,6 +2,10 @@ package gyber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 
 @SpringBootApplication
@@ -11,6 +15,13 @@ public class MainClass {
         SpringApplication.run(MainClass.class , args);
 
         
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
     
 }
