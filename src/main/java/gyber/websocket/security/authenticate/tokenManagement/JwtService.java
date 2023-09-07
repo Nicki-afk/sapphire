4c6f77ac7f2e340d8a4e65c93b61d4488e7fd30a
Nicki-afk <casperplay45@gmail.com>
2023-09-07 15:15:02 +0300

    Добавлен тестовый метод для тестирования

--- src/main/java/gyber/websocket/security/authenticate/tokenManagement/JwtService.java
+++ src/main/java/gyber/websocket/security/authenticate/tokenManagement/JwtService.java
@@ -31,21 +31,18 @@ public class JwtService implements TokenAuthenticate {
 
 
     @Override
-    public String createToken(UserCustomDetails userDetails) {
+    public String createToken(UserCustomDetails userCustomDetails) {
 
-        if(userDetails == null || userDetails.getId() == null || userDetails.getUsername() == null || 
-                            userDetails.getUsername().isEmpty() || userDetails.getCryptoWalletAddress().isEmpty() || userDetails.getCryptoWalletAddress() == null){
+        if(userCustomDetails == null || userCustomDetails.getId() == null || userCustomDetails.getUsername() == null || 
+                            userCustomDetails.getUsername().isEmpty() || userCustomDetails.getCryptoWalletAddress().isEmpty() || userCustomDetails.getCryptoWalletAddress() == null){
             throw new NullPointerException("UserDetails object is null , or filelds in this object is null or empty");
 
         }
        
 
-        // LocalDateTime timeToSet = LocalDateTime.now().plusHours(4);
-        // Date dateToSet = Date.from(timeToSet.atZone(ZoneId.systemDefault()).toInstant());
-
         Map<String , Object>payload = new HashMap<>();
-        payload.put("userId", userDetails.getId());
-        payload.put("username", userDetails.getUsername());
+        payload.put("userId", userCustomDetails.getId());
+        payload.put("username", userCustomDetails.getUsername());
 
         this.expirationDate = Date.from(LocalDateTime.now().plusHours(4).atZone(ZoneId.systemDefault()).toInstant());
 
@@ -65,6 +62,36 @@ public class JwtService implements TokenAuthenticate {
     }
 
 
+    // for test
+    public String testCreateJwt(UserCustomDetails userCustomDetails , long seconds){
+
+        if(userCustomDetails == null || userCustomDetails.getId() == null || userCustomDetails.getUsername() == null || 
+                            userCustomDetails.getUsername().isEmpty() || userCustomDetails.getCryptoWalletAddress().isEmpty() || userCustomDetails.getCryptoWalletAddress() == null){
+            throw new NullPointerException("UserDetails object is null , or filelds in this object is null or empty");
+
+        }
+       
+
+
+        Map<String , Object>payload = new HashMap<>();
+        payload.put("userId", userCustomDetails.getId());
+        payload.put("username", userCustomDetails.getUsername());
+
+        this.expirationDate = Date.from(LocalDateTime.now().plusSeconds(seconds).atZone(ZoneId.systemDefault()).toInstant());
+
+        return   Jwts.
+                         builder()
+                        .signWith(SignatureAlgorithm.HS256 , singature)
+                        .setSubject("jwt")
+                        .setClaims(payload)
+                        .setHeaderParam("type", "JWT")
+                        .setIssuedAt(new Date())
+                        .setIssuer("sapphire-messanger-server")
+                        .setExpiration(this.expirationDate)
+                        .compact();
+    }
+
+
 
 
 
@@ -79,7 +106,7 @@ public class JwtService implements TokenAuthenticate {
               return true;
         
         } catch (JwtException | IllegalArgumentException e) {
-            e.printStackTrace();
+            //e.printStackTrace();
         }   
 
         return false;
@@ -101,12 +128,7 @@ public class JwtService implements TokenAuthenticate {
     }
 
 
-    // for test
-    public void setExpirationDate(long seconds){
-        this.expirationDate = Date.from(LocalDateTime.now().plusSeconds(seconds).atZone(ZoneId.systemDefault()).toInstant());
-
-
-    }
+   
 
     public void setSigningKey(String key){
         this.singature = key;
