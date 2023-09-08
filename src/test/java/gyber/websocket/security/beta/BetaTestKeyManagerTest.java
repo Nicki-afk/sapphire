b794dfb5bcd0cdbaf630f0e6e130b2bdf53cd4b6
Nicki-afk <casperplay45@gmail.com>
2023-09-08 18:38:51 +0300

    Добавлен тестовый класс

---
+++ src/test/java/gyber/websocket/security/beta/BetaTestKeyManagerTest.java
@@ -0,0 +1,15 @@
+package gyber.websocket.security.beta;
+
+import org.junit.jupiter.api.Test;
+
+public class BetaTestKeyManagerTest {
+    @Test
+    void testGenerateMoreKeys() {
+
+    }
+
+    @Test
+    void testIsBetaKeyIsValid() {
+
+    }
+}
