70c1e335798f22b5628bf3b0c154f5aacd1954a9
Nicki-afk <casperplay45@gmail.com>
2023-09-08 18:38:38 +0300

    Добавлены методы. Добавлен царь-ключ

--- src/main/java/gyber/websocket/security/beta/BetaTestKeyManager.java
+++ src/main/java/gyber/websocket/security/beta/BetaTestKeyManager.java
@@ -1,15 +1,28 @@
 package gyber.websocket.security.beta;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import gyber.websocket.models.User;
+import gyber.websocket.models.repo.BetaTestKeyRepository;
 import net.bytebuddy.utility.RandomString;
 
 @Service
 public class BetaTestKeyManager {
-    
 
-    public BetaTestKey[] generateMoreKeys(int quantity){
+    @Autowired
+    private BetaTestKeyRepository repository;
+
+
+    @Value("${tsar.key}}")
+    private String TSAR_KEY;
+
+    public BetaTestKey[] generateMoreKeys(int quantity , String tsarKey){
+
+        if(!TSAR_KEY.equals(tsarKey)){
+            // throws CustomException
+        }
 
         BetaTestKey[]moreKeys = new BetaTestKey[quantity];
         for(int x = 0; x < moreKeys.length; x++){
@@ -22,8 +35,23 @@ public class BetaTestKeyManager {
 
 
     public boolean isBetaKeyIsValid(BetaTestKey betaTestKey){
+        // isActive()
+        // existKey()
         return false;
     }
+
+    private boolean isActive(BetaTestKey betaTestKey){
+        return false;
+
+    }
+
+    private boolean existKey(BetaTestKey betaTestKey){
+        return false;
+    }
+
+
+
+
 
 
 
