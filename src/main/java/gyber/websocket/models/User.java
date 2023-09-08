d2e0ed43386e4d2298eb78409e9c7350bd749f2f
Nicki-afk <casperplay45@gmail.com>
2023-09-08 18:37:36 +0300

    Добавлено OneToOne отношение. Добавлен комментарий

--- src/main/java/gyber/websocket/models/User.java
+++ src/main/java/gyber/websocket/models/User.java
@@ -4,6 +4,7 @@ package gyber.websocket.models;
 
 import javax.persistence.*;
 
+import gyber.websocket.security.beta.BetaTestKey;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -19,22 +20,49 @@ import lombok.ToString;
 @Setter
 @EqualsAndHashCode
 @ToString
+/*
+ * @nic_ko : Добавить обязательно поле Chat 
+ */
 public class User {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "username")         private String userName;
-    @Column(name = "crypto_wallet")    private String cryptoWalletAddress;
-    @Column(name = "pub_key")          private String publicUserKey;
+    @Column(name = "username")
+    private String userName;
+    @Column(name = "crypto_wallet")
+    private String cryptoWalletAddress;
+    @Column(name = "pub_key")
+    private String publicUserKey;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "net_status")
     private NetStatus onlineNetStatus;
+
+    @Column(name = "hash_data")
+    private String hashUserFile;
+
+    // Chats
+    // Messages
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "beta_test_key", referencedColumnName = "id")
+    private BetaTestKey betaTestKey;
+
+    public User(Long id, String userName, String cryptoWalletAddress, String publicUserKey, NetStatus onlineNetStatus,
+            String hashUserFile) {
+        this.id = id;
+        this.userName = userName;
+        this.cryptoWalletAddress = cryptoWalletAddress;
+        this.publicUserKey = publicUserKey;
+        this.onlineNetStatus = onlineNetStatus;
+        this.hashUserFile = hashUserFile;
+    }
+
     
-    @Column(name = "hash_data") private String hashUserFile;
 
 
+    
 
 }
