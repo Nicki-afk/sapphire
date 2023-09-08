299a3e9bf40e34f5d5d075cdc83026ca4f4ec4a6
Nicki-afk <casperplay45@gmail.com>
2023-09-08 18:50:23 +0300

    Добавлена кастомная логика в исключение

--- src/main/java/gyber/websocket/exceptions/BetaTestKeyException.java
+++ src/main/java/gyber/websocket/exceptions/BetaTestKeyException.java
@@ -1,5 +1,21 @@
 package gyber.websocket.exceptions;
 
+import gyber.websocket.security.beta.BetaTestKey;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class BetaTestKeyException extends Exception {
+
+    private BetaTestKey betaTestKey;
+
+    public BetaTestKeyException(String message , BetaTestKey betaTestKey){
+        super(message);
+        this.betaTestKey = betaTestKey;
+
+    }
     
 }
