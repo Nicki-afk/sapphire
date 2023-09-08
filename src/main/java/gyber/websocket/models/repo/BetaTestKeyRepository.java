19a442eaa175bbf9ff54b37eeeec6c6e886fd07e
Nicki-afk <casperplay45@gmail.com>
2023-09-08 18:37:51 +0300

    Создан репозиторий для ключей

---
+++ src/main/java/gyber/websocket/models/repo/BetaTestKeyRepository.java
@@ -0,0 +1,12 @@
+package gyber.websocket.models.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import gyber.websocket.security.beta.BetaTestKey;
+
+@Repository
+public interface BetaTestKeyRepository extends JpaRepository<Long , BetaTestKey> {
+
+    
+}
