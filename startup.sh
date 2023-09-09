45b749f06794e61e9d89fe9cc4fe41fdc424d259
Nicki-afk <casperplay45@gmail.com>
2023-09-09 11:16:56 +0300

    Убраны 2 функций

--- startup.sh
+++ startup.sh
@@ -34,58 +34,10 @@ startMvnw(){
     bash ./mvnw install spring-boot:run
 }
 
-writeAProperties(){
-
-  printLog "Start writing ... "
-  
-  echo "
-
-  spring.datasource.url=jdbc:h2:mem:testdb
-  spring.datasource.driver-class-name=org.h2.Driver
-  spring.datasource.username=sa
-  spring.datasource.password=
-  spring.jpa.database-platform=org.hibernate.dialect.H2Dialect
-  spring.jpa.show-sql=true
-  logging.level.org.hibernate.SQL=DEBUG
-
-  spring.h2.console.enabled=true
-  server.port=8081
-
-  # tokens settings
-  jwt.token.validity.time=14400000
-  jwt.token.sign=jszzGehyVyWZJmalACLgJEJqkykJYsVnipYXdJHtUCckimchXSjszzGehyVyWZJmalACLgJEJqkykJYsVnipYXdJHtUCckimchXS
-  rt.token.sign=fmvBydeuEYhSWiUoimEEwxKyrNGjgRzgyqjSgZRUVOaNcjmfJwfmvBydeuEYhSWiUoimEEwxKyrNGjgRzgyqjSgZRUVOaNcjmfJw
-    
-  
-  
-  " > "application.properties"
-
-  printLog "File application.properties write successful !!! "
-}
 
 
-configurePropertyFile(){
-
-  printLog "Start config property file for test ..."
-  cd src/main/resources
-
-  property_file="application.properties"
-
-  if [ ! -f "$property_file" ]; then
-    printLog "The property file don't exisist"
-    printLog "Creating property file ... "
-    touch "application.properties"
-    printLog "File creating successful !!! Writing a test properties..."
-    writeAProperties
-
-  else
-    printLog "File property exist"
-  fi  
-
-  
 
 
-}
 
 
 printLog "Check Java and Java version ..."
@@ -103,20 +55,17 @@ if [ -z "$java_version" ]; then
   sudo apt update
   sudo apt install -y openjdk-17-jdk
   printLog "Java install successful !!"
-  configurePropertyFile
   startMvnw
 else
   major_version=$(echo "$java_version" | cut -d'.' -f1)
   if [ "$major_version" -eq "17" ]; then
    printLog "Your java version is actual"
-   configurePropertyFile
-  startMvnw
+   startMvnw
   else
     printLog "Java is installed, but it is not version 17. It is version $java_version. Installing the java 17..."
     sudo apt update
     sudo apt install -y openjdk-17-jdk
     printLog "Java install successful !!"
-    configurePropertyFile
     startMvnw
 
   fi
