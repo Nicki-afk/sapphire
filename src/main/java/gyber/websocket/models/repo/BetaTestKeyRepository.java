b8d11d1ee5b539f918820c276884b5afb07a28c6
Nicki-afk <casperplay45@gmail.com>
2023-09-11 15:55:04 +0300

    Создан метод для поиска по id

--- src/main/java/gyber/websocket/models/repo/BetaTestKeyRepository.java
+++ src/main/java/gyber/websocket/models/repo/BetaTestKeyRepository.java
@@ -1,12 +1,16 @@
 package gyber.websocket.models.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import gyber.websocket.security.beta.BetaTestKey;
 
 @Repository
-public interface BetaTestKeyRepository extends JpaRepository<BetaTestKey , BetaTestKey> {
+public interface BetaTestKeyRepository extends JpaRepository<BetaTestKey , Long> {
+
+    Optional<BetaTestKey> findById(Long id);
 
     
 }
