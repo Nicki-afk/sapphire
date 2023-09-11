c18e97c39d6e44621af5a961d3cbd3487140cb0e
Nicki-afk <casperplay45@gmail.com>
2023-09-11 19:01:03 +0300

    Добавлены тестовые сценарий

--- src/test/java/gyber/websocket/security/beta/BetaTestKeyManagerTest.java
+++ src/test/java/gyber/websocket/security/beta/BetaTestKeyManagerTest.java
@@ -1,15 +1,70 @@
 package gyber.websocket.security.beta;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import gyber.websocket.exceptions.BetaTestKeyException;
+import gyber.websocket.models.repo.BetaTestKeyRepository;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class BetaTestKeyManagerTest {
+
+
+    @Autowired
+    private BetaTestKeyManager betaTestKeyManager;
+
+    
+
     @Test
-    void testGenerateMoreKeys() {
+    public void testExistTsarKey(){
+        assertNotNull(betaTestKeyManager.getTSAR_KEY());
 
     }
 
-    @Test
-    void testIsBetaKeyIsValid() {
+
+    // testGenerateMoreKeys
+
+   @Test
+   public void testGenerateMoreKeysInNormalScript() throws BetaTestKeyException {
+      
+        BetaTestKey[]arrKeys = this.betaTestKeyManager.testGenerateMoreKeys(30, this.betaTestKeyManager.getTSAR_KEY());
+        assertTrue("Arr length not 30", (arrKeys.length == 30));
+
 
     }
+
+    @Test(expected = BetaTestKeyException.class)
+    public void testGenerateMoreKeysInIncorrectTsarKey() throws BetaTestKeyException{
+        BetaTestKey[]arrKeys = this.betaTestKeyManager.testGenerateMoreKeys(30, "Incorrect tsar key");
+         
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGenerateMoreKeysInIncorrectNumber() throws BetaTestKeyException{
+        BetaTestKey[]arrKeys = this.betaTestKeyManager.testGenerateMoreKeys(-30, "Incorrect tsar key");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGenerateMoreKeysInEmptyString() throws BetaTestKeyException{
+        BetaTestKey[]arrKeys = this.betaTestKeyManager.testGenerateMoreKeys(30, "");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGenerateMoreKeysInNullString() throws BetaTestKeyException{
+        BetaTestKey[]arrKeys = this.betaTestKeyManager.testGenerateMoreKeys(30, null);
+    }
+
+
+    
 }
