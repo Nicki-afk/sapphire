3f718ab0fee084c1213af109f91e8f0bc25b2405
Nicki-afk <casperplay45@gmail.com>
2023-09-13 11:42:45 +0300

    Добавлен test-property файл для тестов

--- src/test/java/gyber/websocket/security/beta/BetaTestKeyManagerTest.java
+++ src/test/java/gyber/websocket/security/beta/BetaTestKeyManagerTest.java
@@ -1,5 +1,6 @@
 package gyber.websocket.security.beta;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import gyber.websocket.exceptions.BetaTestKeyException;
@@ -18,6 +20,7 @@ import gyber.websocket.models.repo.BetaTestKeyRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource("classpath:test-source.properties")
 public class BetaTestKeyManagerTest {
 
 
@@ -50,33 +53,71 @@ public class BetaTestKeyManagerTest {
          
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test(expected = BetaTestKeyException.class)
     public void testGenerateMoreKeysInIncorrectNumber() throws BetaTestKeyException{
-        BetaTestKey[]arrKeys = this.betaTestKeyManager.testGenerateMoreKeys(-30, "Incorrect tsar key");
+        BetaTestKey[]arrKeys = this.betaTestKeyManager.testGenerateMoreKeys(-30, this.betaTestKeyManager.getTSAR_KEY());
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test(expected = BetaTestKeyException.class)
     public void testGenerateMoreKeysInEmptyString() throws BetaTestKeyException{
         BetaTestKey[]arrKeys = this.betaTestKeyManager.testGenerateMoreKeys(30, "");
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test(expected = BetaTestKeyException.class)
     public void testGenerateMoreKeysInNullString() throws BetaTestKeyException{
         BetaTestKey[]arrKeys = this.betaTestKeyManager.testGenerateMoreKeys(30, null);
     }
 
+    /*
+     * Тест ниже проверяет не просрочены ли сгенерированные ключи. Тестовый ключ валиден 1 минуту 
+     * тест выполняется 59 секунд , тестовые ключи должны быть валидны еще 1 секунду.
+    */
     @Test
     public void testIsBetaKeyIsValidTestExpirationValid() {
     
 
         try{
             BetaTestKey[]arrKeys = this.betaTestKeyManager.testGenerateMoreKeys(30, this.betaTestKeyManager.getTSAR_KEY());
+            this.betaTestKeyManager.saveMoreKeys(arrKeys);
+
             Thread.sleep(59000);
 
             int x = 0;
             while(x < arrKeys.length){
 
-               // assertTrue("null", this.betaTestKeyManager.);
+                String key = arrKeys[x].getKey();
+                assertTrue("Key is not valid", this.betaTestKeyManager.isBetaKeyIsValid(key));
+                x++;
+            }
+
+        }catch(Exception  e){
+            e.printStackTrace();
+
+        }
+
+
+    }
+
+
+     /*
+     * Тест ниже проверяет не просрочены ли сгенерированные ключи. Тестовый ключ валиден 1 минуту 
+     * тест выполняется 61 секунду , тестовые ключи уже не валидны 1 секунду и не могут быть использованы  
+     */
+    @Test
+    public void testIsBetaKeyIsValidTestExpirationInvalid() {
+    
+
+        try{
+            BetaTestKey[]arrKeys = this.betaTestKeyManager.testGenerateMoreKeys(30, this.betaTestKeyManager.getTSAR_KEY());
+            this.betaTestKeyManager.saveMoreKeys(arrKeys);
+
+            Thread.sleep(61000);
+
+            int x = 0;
+            while(x < arrKeys.length){
+
+                String key = arrKeys[x].getKey();
+                assertFalse("Key is valid but key should not valid", this.betaTestKeyManager.isBetaKeyIsValid(key));
                 x++;
             }
 
