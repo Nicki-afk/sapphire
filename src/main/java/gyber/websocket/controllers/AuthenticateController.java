955f9409910c8937d1dc1d9147551fb91f2ef007
Nicki-afk <casperplay45@gmail.com>
2023-09-15 21:41:30 +0300

    Оптимизирован метод авторизации

--- src/main/java/gyber/websocket/controllers/AuthenticateController.java
+++ src/main/java/gyber/websocket/controllers/AuthenticateController.java
@@ -1,9 +1,13 @@
 package gyber.websocket.controllers;
 
 import java.time.LocalDateTime;
+import java.util.Base64;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
@@ -14,11 +18,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.JsonSerializable.Base;
+
 import gyber.websocket.models.UserCustomDetailsService;
+import gyber.websocket.models.repo.UserRepository;
 import gyber.websocket.security.authenticate.tokenManagement.JwtService;
+import gyber.websocket.security.authenticate.tokenManagement.TokenLocalStorageManager;
 import gyber.websocket.security.authenticate.tokenManagement.TokenPairObject;
 import io.jsonwebtoken.JwtException;
 import gyber.websocket.exceptions.ErrorRestResponse;
@@ -27,46 +36,55 @@ import gyber.websocket.models.NetStatus;
 import gyber.websocket.models.User;
 import gyber.websocket.models.UserCustomDetails;
 
-
-
 @Controller
 @RequestMapping("/auth")
 public class AuthenticateController {
 
-
     @Autowired
     private UserCustomDetailsService userIPFSCustomDetailsService;
 
+    @Autowired
+    private UserRepository repository;
+
 
     @Autowired
-    private JwtService jwtService;
+    private TokenLocalStorageManager tokenManager;
 
-    // Map ( singuture , cryptoWalletAdress , pub key)
+
+
     @PostMapping
-    public ResponseEntity authenticateUser(@RequestBody Map<String, String>userAuthData){
-        
-        String signature = userAuthData.get("signature");  
-        String cryptoWalletAddress = userAuthData.get("crypto_wallet_address");
+    public ResponseEntity authenticateUser(
+            @RequestHeader("Authorization") String authenticateData) throws TokenLocalStorageException {
 
-      //  UserDetails userDetails = userIPFSCustomDetailsService.loadUserByUsername(cryptoWalletAddress);
-      UserCustomDetails userDetails = this.userIPFSCustomDetailsService.loadUserByCryptowalletAddress(cryptoWalletAddress);
+        String decodeBase64String = new String(Base64.getDecoder()
+                .decode((authenticateData.substring(authenticateData.indexOf(" "))).trim().getBytes()));
+        String[] arr = decodeBase64String.split(":");
 
 
-        UsernamePasswordAuthenticationToken userCredentalsData = new UsernamePasswordAuthenticationToken(userDetails.getUsername() , null);
+        User user = this.repository.findByCryptoWalletAddress(arr[1]).orElse(null);
+        UserCustomDetails userDetails = this.userIPFSCustomDetailsService.loadUserByCryptowalletAddress(arr[1]);
+
+        UsernamePasswordAuthenticationToken userCredentalsData = new UsernamePasswordAuthenticationToken(
+                userDetails.getUsername(), null);
         SecurityContextHolder.getContext().setAuthentication(userCredentalsData);
 
-        String token = jwtService.createToken(userDetails);
-
+        TokenPairObject tokenPairObject = this.tokenManager.addTokenPairForUser(user);
 
         return ResponseEntity.ok(
-            Map.of("jwt" , token , "time" , LocalDateTime.now())
-        );
-
+                Map.of("pair_object" , tokenPairObject ,  "time", LocalDateTime.now()));
 
     }
 
+    @GetMapping
+    public ResponseEntity getAuthenticateInfo() {
 
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("WWW-Authenticate",
+                "GyberAuth realm=\"Gyber Authenticate\", instructions=\"Enter the public key, your signature and your wallet address.\"");
+        return ResponseEntity.status(401)
+                .headers(httpHeaders)
+                .build();
 
-    
-    
+    }
+
 }
