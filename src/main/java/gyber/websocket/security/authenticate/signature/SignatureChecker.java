5b103cfea2991ed8d7eefac37fcc6f22f15642ac
Nicki-afk <casperplay45@gmail.com>
2023-09-20 22:16:01 +0300

    Добавлены методы для обрезания данных

--- src/main/java/gyber/websocket/security/authenticate/signature/SignatureChecker.java
+++ src/main/java/gyber/websocket/security/authenticate/signature/SignatureChecker.java
@@ -1,13 +1,44 @@
 package gyber.websocket.security.authenticate.signature;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignatureChecker {
 
 
-    public boolean verifySignature(String publicBase64key , String singBase64){
+    private String algorithm = "SHA256withECDSA";
+
+    
+    @PostConstruct
+    public void initComponent(){
+        /// ... 
+    }
+
+
+
+
+
+    public boolean verifySignature(String base64AuthenticateData){
         return false;
+    }
+
+
+    public byte[] obtainSignature(String decodeBase64String){
+
+        return new byte[]{};
+
+    }
+
+    public byte[] obtainPublicKey(String decodeBase64String){
+
+        return new byte[]{};
+    }
+
+    public byte[] obtainWallet(String decodeBase64String){
+        
+        return new byte[]{};
     }
     
 }
