daf881d32cffd8adf7c4ee2c89d1daad24238d58
Nicki-afk <casperplay45@gmail.com>
2023-09-20 21:42:43 +0300

    Добавлены методы для удаления ключей

--- src/main/java/gyber/websocket/controllers/SystemController.java
+++ src/main/java/gyber/websocket/controllers/SystemController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,9 +38,46 @@ public class SystemController {
             )
         );
 
-        
+    }
 
 
+    @DeleteMapping("/key/del")
+    public ResponseEntity deleteKey(
+        @RequestParam("tsar") String tsarKey , 
+        @RequestParam("keyId") Long keyId 
+    ){
+
+        // ... 
+
+        return ResponseEntity.ok().build();
+
+    }
+
+
+    /*
+     * Удаляет ключи которые были сгенерированы в 
+     * определенную дату 
+     */
+    @DeleteMapping("/key/del/date")
+    public ResponseEntity deleteKeyByDate(
+        @RequestParam("tsar") String tsarKey , 
+        @RequestParam("date") String date 
+    ){
+
+        // ... 
+        return ResponseEntity.ok().build();
+
+    }
+
+
+    @DeleteMapping("/key/del/all")
+    public ResponseEntity deleteAllKeys(
+        @RequestParam("tsar") String tsarKey
+    ){
+
+        /// ... 
+
+        return ResponseEntity.ok().build();
     }
 
 
