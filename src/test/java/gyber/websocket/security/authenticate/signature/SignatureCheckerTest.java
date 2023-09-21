4e72b4cab00172516d38528b3ab6ca697f94fefc
Nicki-afk <casperplay45@gmail.com>
2023-09-21 22:27:29 +0300

    Написана базовая проврка подписи

--- src/test/java/gyber/websocket/security/authenticate/signature/SignatureCheckerTest.java
+++ src/test/java/gyber/websocket/security/authenticate/signature/SignatureCheckerTest.java
@@ -2,9 +2,26 @@ package gyber.websocket.security.authenticate.signature;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import java.security.Signature;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.ECKeyPair;
+import org.web3j.crypto.Hash;
+import org.web3j.crypto.Keys;
+import org.web3j.crypto.Sign;
+import org.web3j.utils.Numeric;
 
 @SpringBootTest(classes = {SignatureChecker.class})
 public class SignatureCheckerTest {
@@ -53,6 +70,36 @@ public class SignatureCheckerTest {
 
     @Test
     public void testVerifySignature() {
+
+        try {
+            String sig = "0x5942c9b4572c9342a5294223ed71cc0c6b6d87b50b8c487545535aa6a1fe380f79f36db9475d4255e99c27461213d8b6ade22819ab227a4a6607a102aaf7a6cb1b";
+            String message = "\u0019Ethereum Signed Message:\n" + "Hello World".length() + "Hello World";
+            String address = "0x28161A5F4A32C67fFb17e9bA722ddB8B342A970c";
+
+            byte[] signatureBytes = Numeric.hexStringToByteArray(sig);
+
+            byte[] r = new byte[32];
+            byte[] s = new byte[32];
+            byte v = signatureBytes[64];
+
+            System.arraycopy(signatureBytes, 0, r, 0, 32);
+            System.arraycopy(signatureBytes, 32, s, 0, 32);
+
+            Sign.SignatureData signatureData = new Sign.SignatureData(v, r, s);
+
+            BigInteger pubKey = Sign.signedMessageToKey(message.getBytes(), signatureData);
+
+            String computedAddress = "0x" + Keys.getAddress(pubKey);
+            if (computedAddress.equalsIgnoreCase(address)) {
+                System.out.println("Signature verified!");
+            } else {
+                System.out.println("Invalid signature!");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }   
+        
 
     }
 }
