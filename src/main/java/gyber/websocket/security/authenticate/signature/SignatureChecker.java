39cb4d13e6c77de1a13e0d185168805abea6b3a9
Nicki-afk <casperplay45@gmail.com>
2023-09-21 23:01:40 +0300

    Закоментирован код

--- src/main/java/gyber/websocket/security/authenticate/signature/SignatureChecker.java
+++ src/main/java/gyber/websocket/security/authenticate/signature/SignatureChecker.java
@@ -27,29 +27,34 @@ public class SignatureChecker {
     public static boolean verifySignature(String address , String signature , String base64MessageHash) {
 
         try{
+
+        // Преобразуем подпись из шестнадцатеричной в байтовый массив
         byte[] signatureBytes = Numeric.hexStringToByteArray(signature);
+
+          // Выделяем r, s и v из подписи
         byte[] r = new byte[32];
         byte[] s = new byte[32];
         byte v = signatureBytes[64];
         
 
+        // Исходное сообщение с префиксом, который обычно добавляется MetaMask декодированое Base64
         String decodeMsg = new String(Base64.getDecoder().decode(base64MessageHash));
 
     
         // Копируем байты в соответствующие массивы
         System.arraycopy(signatureBytes, 0, r, 0, 32);
         System.arraycopy(signatureBytes, 32, s, 0, 32);
+ 
+        // Создаем объект подписи
+        Sign.SignatureData signatureData = new Sign.SignatureData(v, r, s);
 
-         Sign.SignatureData signatureData = new Sign.SignatureData(v, r, s);
+        // Восстанавливаем публичный ключ из подписи и сообщения
+        BigInteger pubKey = Sign.signedMessageToKey(decodeMsg.getBytes(), signatureData);
 
-            BigInteger pubKey = Sign.signedMessageToKey(decodeMsg.getBytes(), signatureData);
-
-            String computedAddress = "0x" + Keys.getAddress(pubKey);
-            if (computedAddress.equalsIgnoreCase(address)) {
-                return true;
-            } else {
-                return false;
-            }
+        // Получаем адрес из публичного ключа
+        String computedAddress = "0x" + Keys.getAddress(pubKey);
+        
+        return computedAddress.equalsIgnoreCase(address);
 
 
         }catch(Exception e){
