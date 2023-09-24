add4f456fe32d5028a67d49062a3ba47f8c1dd0a
Nicki-afk <casperplay45@gmail.com>
2023-09-24 22:50:59 +0300

    Удалены комментарий. Добавлены тестовые методы

--- src/test/java/gyber/websocket/security/authenticate/signature/SignatureCheckerTest.java
+++ src/test/java/gyber/websocket/security/authenticate/signature/SignatureCheckerTest.java
@@ -2,11 +2,15 @@ package gyber.websocket.security.authenticate.signature;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.refEq;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Base64;
 
+import org.apache.el.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,49 +19,23 @@ public class SignatureCheckerTest {
 
 
 
+
+
+
+    public Stream getMoreParams(){
+
+        return null; 
+
+    }
+
+
+
+
+
+
     @Test
     public void testVerifySignature() throws UnsupportedEncodingException {
 
-        // try {
-        //     String sig = "0x5942c9b4572c9342a5294223ed71cc0c6b6d87b50b8c487545535aa6a1fe380f79f36db9475d4255e99c27461213d8b6ade22819ab227a4a6607a102aaf7a6cb1b";
-        //     String message = "\u0019Ethereum Signed Message:\n" + "Hello World".length() + "Hello World";
-        //     String address = "0x28161A5F4A32C67fFb17e9bA722ddB8B342A970c";
-
-        //     byte[] signatureBytes = Numeric.hexStringToByteArray(sig);
-
-        //     byte[] r = new byte[32];
-        //     byte[] s = new byte[32];
-        //     byte v = signatureBytes[64];
-
-        //     System.arraycopy(signatureBytes, 0, r, 0, 32);
-        //     System.arraycopy(signatureBytes, 32, s, 0, 32);
-
-        //     Sign.SignatureData signatureData = new Sign.SignatureData(v, r, s);
-
-        //     BigInteger pubKey = Sign.signedMessageToKey(message.getBytes(), signatureData);
-
-        //     String computedAddress = "0x" + Keys.getAddress(pubKey);
-        //     if (computedAddress.equalsIgnoreCase(address)) {
-        //         System.out.println("Signature verified!");
-        //     } else {
-        //         System.out.println("Invalid signature!");
-        //     }
-
-        // } catch (Exception e) {
-        //     e.printStackTrace();
-        // }   
-
-
-        // assertTrue(
-
-        
-        //     SignatureChecker.verifySignature(
-        //      "0x28161A5F4A32C67fFb17e9bA722ddB8B342A970c" , 
-        //      "0x5942c9b4572c9342a5294223ed71cc0c6b6d87b50b8c487545535aa6a1fe380f79f36db9475d4255e99c27461213d8b6ade22819ab227a4a6607a102aaf7a6cb1b" , 
-        //      (Base64.getEncoder().encodeToString(("\u0019Ethereum Signed Message:\n" + "Hello World".length() + "Hello World").getBytes()))    
-            
-        //     )
-        // );
 
         String base64Signature = "MHhlY2FmZmNlMjliMGM4MjFlOTRmNDkzNzgxODk4MWUzMjYyNzI3ZmE2Mzg0OTMwNTI2NTFmMzk4YzZkMzEzMWI0NjZjODUxMmQxYmYwZWVkNDFmOTg3OGRlMDJmZjM0YmYzNmMwMTRmZTRjZmNmNzAyZTIwNjUyZTRiMDg2MzIxODFj";
         String message = "\u0019Ethereum Signed Message:\n" + "Hello World".length() + "Hello World";
@@ -77,13 +55,23 @@ public class SignatureCheckerTest {
                                 .recoveryWalletAddress()
                                 .build();
 
-                               //System.out.println(checker);
-                                System.out.println("2 : "  + SignatureChecker.verifySignature("0x7e72Dc9D21f749Eb7E669Eb834f9b112AD9Babec" , "0xecaffce29b0c821e94f4937818981e3262727fa638493052651f398c6d3131b466c8512d1bf0eed41f9878de02ff34bf36c014fe4cfcf702e20652e4b08632181c" , base64Message) );
+        boolean result = checker.verifySignature();
 
-       // assertTrue(result);
 
-       System.out.println("Result : " + checker.verifySignature());
+        assertTrue(result);
+
+    }
+
+
+    @Test
+    public void testVerifySignatureInInvalidSignature(){
+
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("getMoreParams")
+    public void testVerifySignatureInMoreParameters(){
         
-
     }
 }
