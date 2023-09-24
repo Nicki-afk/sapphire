514ac2c3a84b8b7946544c1bdc56b41c4789f22e
Nicki-afk <casperplay45@gmail.com>
2023-09-24 22:20:35 +0300

    Переписан метод тестирования

--- src/test/java/gyber/websocket/security/authenticate/signature/SignatureCheckerTest.java
+++ src/test/java/gyber/websocket/security/authenticate/signature/SignatureCheckerTest.java
@@ -3,7 +3,7 @@ package gyber.websocket.security.authenticate.signature;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-
+import java.io.UnsupportedEncodingException;
 import java.util.Base64;
 
 import org.junit.jupiter.api.Test;
@@ -16,7 +16,7 @@ public class SignatureCheckerTest {
 
 
     @Test
-    public void testVerifySignature() {
+    public void testVerifySignature() throws UnsupportedEncodingException {
 
         // try {
         //     String sig = "0x5942c9b4572c9342a5294223ed71cc0c6b6d87b50b8c487545535aa6a1fe380f79f36db9475d4255e99c27461213d8b6ade22819ab227a4a6607a102aaf7a6cb1b";
@@ -48,16 +48,41 @@ public class SignatureCheckerTest {
         // }   
 
 
-        assertTrue(
+        // assertTrue(
 
         
-            SignatureChecker.verifySignature(
-             "0x28161A5F4A32C67fFb17e9bA722ddB8B342A970c" , 
-             "0x5942c9b4572c9342a5294223ed71cc0c6b6d87b50b8c487545535aa6a1fe380f79f36db9475d4255e99c27461213d8b6ade22819ab227a4a6607a102aaf7a6cb1b" , 
-             (Base64.getEncoder().encodeToString(("\u0019Ethereum Signed Message:\n" + "Hello World".length() + "Hello World").getBytes()))    
+        //     SignatureChecker.verifySignature(
+        //      "0x28161A5F4A32C67fFb17e9bA722ddB8B342A970c" , 
+        //      "0x5942c9b4572c9342a5294223ed71cc0c6b6d87b50b8c487545535aa6a1fe380f79f36db9475d4255e99c27461213d8b6ade22819ab227a4a6607a102aaf7a6cb1b" , 
+        //      (Base64.getEncoder().encodeToString(("\u0019Ethereum Signed Message:\n" + "Hello World".length() + "Hello World").getBytes()))    
             
-            )
-        );
+        //     )
+        // );
+
+        String base64Signature = "MHhlY2FmZmNlMjliMGM4MjFlOTRmNDkzNzgxODk4MWUzMjYyNzI3ZmE2Mzg0OTMwNTI2NTFmMzk4YzZkMzEzMWI0NjZjODUxMmQxYmYwZWVkNDFmOTg3OGRlMDJmZjM0YmYzNmMwMTRmZTRjZmNmNzAyZTIwNjUyZTRiMDg2MzIxODFj";
+        String message = "\u0019Ethereum Signed Message:\n" + "Hello World".length() + "Hello World";
+        String base64Message = Base64.getEncoder().encodeToString(message.getBytes("UTF-8"));
+        
+
+        String base64Address = "MHg3ZTcyRGM5RDIxZjc0OUViN0U2NjlFYjgzNGY5YjExMkFEOUJhYmVj";
+
+       SignatureChecker checker = SignatureChecker
+                                .builder()  
+                                .setInputBase64Signature(base64Signature)
+                                .setInputBase64Message(base64Message)
+                                .setInputBase64WalletAddress(base64Address)
+                                .copyBytes()
+                                .createSignatureObject()
+                                .recoveryPublicKey()
+                                .recoveryWalletAddress()
+                                .build();
+
+                               //System.out.println(checker);
+                                System.out.println("2 : "  + SignatureChecker.verifySignature("0x7e72Dc9D21f749Eb7E669Eb834f9b112AD9Babec" , "0xecaffce29b0c821e94f4937818981e3262727fa638493052651f398c6d3131b466c8512d1bf0eed41f9878de02ff34bf36c014fe4cfcf702e20652e4b08632181c" , base64Message) );
+
+       // assertTrue(result);
+
+       System.out.println("Result : " + checker.verifySignature());
         
 
     }
