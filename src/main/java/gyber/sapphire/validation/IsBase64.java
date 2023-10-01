e17fee39ac29a00ea1b5e0fcf0d49f9176e06031
Nicki-afk <casperplay45@gmail.com>
2023-10-01 20:57:57 +0300

    Написанна собственная аннотация для валидаций

---
+++ src/main/java/gyber/sapphire/validation/IsBase64.java
@@ -0,0 +1,22 @@
+package gyber.sapphire.validation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target({ElementType.FIELD , ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = IsBase64Validator.class)
+public @interface IsBase64 {
+
+    String message() default "The string is not a valid Base64 string";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+    
+}
