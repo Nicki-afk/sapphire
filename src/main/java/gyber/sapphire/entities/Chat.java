7939f7433dc5060aefedb23672133666d1ce2409
Nicki-afk <casperplay45@gmail.com>
2023-10-02 23:13:55 +0300

    Добавлена валидация

--- src/main/java/gyber/sapphire/entities/Chat.java
+++ src/main/java/gyber/sapphire/entities/Chat.java
@@ -2,9 +2,14 @@ package gyber.sapphire.entities;
 
 import java.time.LocalDateTime;
 import java.util.Queue;
+import java.util.Set;
 
 import javax.persistence.*;
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,16 +19,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Entity
+@Table(name = "chats")
 public class Chat {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long chatId;
 
-    @Column(name = "time_to_create") private LocalDateTime dateAndTimeToCreateChat;
+    @Column(name = "time_to_create") @NotNull private LocalDateTime dateAndTimeToCreateChat;
 
-    @Column(name = "companion_one") private Long user1;
-    @Column(name = "companion_two") private Long user2;
+    @Column(name = "companion_one") @NotNull @Min(1) private Long user1;
+    @Column(name = "companion_two") @NotNull @Min(1) private Long user2;
+
+    @OneToMany(mappedBy = "chat")
+    private Set<Message>messageList;
+
+
 
     // @ManyToOne @Valid private User userOne;
     // @ManyToOne @Valid private User userTwo;
--- src/main/java/gyber/sapphire/entities/Message.java
+++ src/main/java/gyber/sapphire/entities/Message.java
@@ -1,6 +1,18 @@
 package gyber.sapphire.entities;
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,15 +20,30 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "messages")
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
-    private String from;
-    private String to;
+
+    @Column @NotBlank @Size(min = 6 , max = 8) private String from;
+    @Column @NotBlank @Size(min = 6 , max = 8) private String to;
+
+    @Column(name = "message_hash")
+    @NotBlank 
     private String content;
-    private LocalDateTime dateSent;
-    private LocalDateTime dateReceived;
-    private Boolean isRead = false;
-    private Boolean isDelivered = false;
+
+    @Column @NotNull private LocalDateTime dateSent;
+    @Column @NotNull private LocalDateTime dateReceived;
+    @Column @NotNull private Boolean isRead = false;
+    @Column @NotNull private Boolean isDelivered = false;
+
+
+    @ManyToOne
+    @JoinColumn(name = "chat" , nullable = false)
+    private Chat chat;
 
  
 
