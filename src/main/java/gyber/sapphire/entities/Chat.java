fb02a6a82ccd84fe888e04f771943118d83345e7
Nicki-afk <casperplay45@gmail.com>
2023-10-02 22:52:26 +0300

    Предварительная версия объекта

--- src/main/java/gyber/sapphire/entities/Chat.java
+++ src/main/java/gyber/sapphire/entities/Chat.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.Queue;
 
 import javax.persistence.*;
+import javax.validation.Valid;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,10 +20,17 @@ public class Chat {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long chatId;
 
-    private LocalDateTime dateAndTimeToCreateChat;
+    @Column(name = "time_to_create") private LocalDateTime dateAndTimeToCreateChat;
 
-    private String nickNamePersonOne;
-    private String nickNamePersonTwo;
+    @Column(name = "companion_one") private Long user1;
+    @Column(name = "companion_two") private Long user2;
+
+    // @ManyToOne @Valid private User userOne;
+    // @ManyToOne @Valid private User userTwo;
+
+
+    // private String nickNamePersonOne;
+    // private String nickNamePersonTwo;
    // private Queue<Message> queueMessages;
 
     
