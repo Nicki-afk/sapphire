15220be525156d1a1ec9633ae335f74b032b85a8
Nicki-afk <casperplay45@gmail.com>
2023-10-14 14:31:40 +0300

    Write new methods in repository

--- src/main/java/gyber/sapphire/database/repositories/UserRepository.java
+++ src/main/java/gyber/sapphire/database/repositories/UserRepository.java
@@ -3,13 +3,27 @@ package gyber.sapphire.database.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import gyber.sapphire.profile.User;
+import java.util.List;
+import gyber.sapphire.beta.BetaTestKey;
+
+
 
 public interface UserRepository extends JpaRepository<User , Long>{
 
     Optional<User> findById(Long id);
     Optional<User> findByUserName(String userName);
     Optional<User> findByCryptoWalletAddress(String cryptoWalletAddress);
+
+    Optional<User> findByHashUserFile(String hashUserFile);
+
+
+    @Query("SELECT user FROM User user WHERE user.betaTestKey.key = :key")
+    Optional<User> findByBetaTestKey(String key);
+
+
+
     
 }
