1084227e98c1aa6c9bc7f57770bc1c9da093e247
Nicki-afk <casperplay45@gmail.com>
2023-10-14 17:14:36 +0300

    wirte a simple test

--- src/test/java/gyber/sapphire/database/repositories/UserRepositoryTest.java
+++ src/test/java/gyber/sapphire/database/repositories/UserRepositoryTest.java
@@ -1,17 +1,41 @@
 package gyber.sapphire.database.repositories;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import gyber.sapphire.profile.NetStatus;
+import gyber.sapphire.profile.User;
 
 
 @DataJpaTest
 @TestPropertySource("classpath:test-source.properties")
+@Rollback(value = false)
 public class UserRepositoryTest {
 
     @Autowired
-    private UserRepository userRepository;
+    private  UserRepository userRepository;
+
+
+    @BeforeEach
+    public void moreUsers(){
+        User user = new User( "@nic_ko", "0xLK7HC8w99c9qwkjhebnlc9Qd1", "kjcwbiuofhwiufhbwkv", NetStatus.ONLINE,  "wouhiuhciuhech92hdiuhgefigijhgx9ubwxihgqufgq");
+        this.userRepository.save(user);
+
+        System.out.println("\n\n\n " + "OK " + "\n\n\n");
+    }
+  
+
+   
 
 
 
@@ -32,7 +56,10 @@ public class UserRepositoryTest {
 
     @Test
     void testFindById() {
+      //  moreUsers();
+        assertNotNull((userRepository.findById(1L).get()));
 
+    
     }
 
     @Test
