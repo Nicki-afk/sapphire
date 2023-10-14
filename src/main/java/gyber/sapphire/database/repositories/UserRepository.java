9b36915adc6f60dbe897442a7b1a551f9fb6085a
Nicki-afk <casperplay45@gmail.com>
2023-10-14 16:18:36 +0300

    Added CRUD methods in Repository

--- src/main/java/gyber/sapphire/database/repositories/UserRepository.java
+++ src/main/java/gyber/sapphire/database/repositories/UserRepository.java
@@ -1,29 +1,44 @@
 package gyber.sapphire.database.repositories;
 
 import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import gyber.sapphire.profile.User;
-import java.util.List;
-import gyber.sapphire.beta.BetaTestKey;
+import gyber.sapphire.profile.NetStatus;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface UserRepository extends JpaRepository<User, Long> {
 
-
-public interface UserRepository extends JpaRepository<User , Long>{
-
+    // READ
     Optional<User> findById(Long id);
+
     Optional<User> findByUserName(String userName);
+
     Optional<User> findByCryptoWalletAddress(String cryptoWalletAddress);
 
     Optional<User> findByHashUserFile(String hashUserFile);
 
-
     @Query("SELECT user FROM User user WHERE user.betaTestKey.key = :key")
     Optional<User> findByBetaTestKey(String key);
 
+    // UPDATE
+    @Query("UPDATE User u SET u.hashUserFile = :hashFile WHERE u.id = :userId")
+    void updateHashUserFile(Long userId, String hashFile);
 
+    @Query("UPDATE User u SET u.username = :newUserName WHERE u.id = :userId")
+    void updateUserName(Long userId, String newUserName);
 
-    
+    @Query("UPDATE User u SET u.cryptoWalletAddress = :newCryptoWalletAddress WHERE u.id = :userId")
+    void updateCryptoWalletAddress(Long userId, String newCryptoWalletAddress);
+
+    @Query("UPDATE User u SET u.onlineNetStatus = :newStatus WHERE u.id = :userId")
+    void updateNetStatus(Long userId, @Param("newStatus") NetStatus newStatus);
+
+    // DELETE
+    void deleteById(Long id);
+
+    void deleteByUserName();
+
+    void deleteByCryptoWalletAddress();
+
 }
