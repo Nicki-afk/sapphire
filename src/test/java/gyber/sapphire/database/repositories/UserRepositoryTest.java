37f83818c9a812ec57354c97c77223567f1593b1
Nicki-afk <casperplay45@gmail.com>
2023-10-14 17:37:06 +0300

    Added logic which generate more Users objects

--- src/test/java/gyber/sapphire/database/repositories/UserRepositoryTest.java
+++ src/test/java/gyber/sapphire/database/repositories/UserRepositoryTest.java
@@ -3,6 +3,8 @@ package gyber.sapphire.database.repositories;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import org.assertj.core.internal.bytebuddy.utility.RandomString;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,14 +28,45 @@ public class UserRepositoryTest {
     private  UserRepository userRepository;
 
 
+
+
     @BeforeEach
     public void moreUsers(){
-        User user = new User( "@nic_ko", "0xLK7HC8w99c9qwkjhebnlc9Qd1", "kjcwbiuofhwiufhbwkv", NetStatus.ONLINE,  "wouhiuhciuhech92hdiuhgefigijhgx9ubwxihgqufgq");
-        this.userRepository.save(user);
 
-        System.out.println("\n\n\n " + "OK " + "\n\n\n");
-    }
+
+        for(int x = 0; x < 2; x++ ){
+
+            this.userRepository
+            .save(
+                (
+                new User(
+                    ( "@" + new RandomString(5).nextString()) , 
+                    ("0x" + new RandomString(16).nextString()) , 
+                    (new RandomString(128).nextString()) , 
+                    NetStatus.ONLINE , 
+                    (new RandomString(200).nextString().toUpperCase())
+                ))
+            );
+            System.out.println("USER SAVE ");
+
+
+
+        }
+
+        System.out.println(
+            "\n\n\n\n Users Saved  : "
+            + 
+            (this.userRepository.count())
+            +
+            "\n\n\n\n"
+        );
+
+
   
+    }
+
+
+
 
    
 
@@ -67,3 +100,4 @@ public class UserRepositoryTest {
 
     }
 }
+
