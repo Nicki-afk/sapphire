012c74a7a6c849b5fd660e61f9434e7eca13d118
Nicki-afk <casperplay45@gmail.com>
2023-11-13 21:06:42 +0200

    rewrite Message entity

--- src/main/java/gyber/sapphire/messaging/Message.java
+++ src/main/java/gyber/sapphire/messaging/Message.java
@@ -11,7 +11,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,28 +25,31 @@ public class Message {
 
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "msg_id")
+    private Long msgID;
 
-    @Column(name = "fromH") @NotBlank @Size(min = 6 , max = 8) private String from;
-    @Column(name = "toH") @NotBlank @Size(min = 6 , max = 8) private String to;
-
+ 
     @Column(name = "message_hash")
     @NotBlank 
     private String content;
 
-    @Column @NotNull private LocalDateTime dateSent;
-    @Column @NotNull private LocalDateTime dateReceived;
-    @Column @NotNull private Boolean isRead = false;
-    @Column @NotNull private Boolean isDelivered = false;
+    @Column @NotNull private LocalDateTime sentAt;
 
 
     @ManyToOne
     @JoinColumn(name = "chat" , nullable = false)
     private Chat chat;
 
- 
+
+    public Message(@NotBlank String content, Chat chat) {
+        this.content = content;
+        this.chat = chat;
+    }
 
 
+    public Message(@NotBlank String content) {
+        this.content = content;
+    }
 
 
 }
