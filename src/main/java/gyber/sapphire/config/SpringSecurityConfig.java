2d1f51c080f64a9ee360fb8f878107bab16ad261
Nicki-afk <casperplay45@gmail.com>
2023-11-16 11:04:16 +0200

    restore to default settings Spring security

--- src/main/java/gyber/sapphire/config/SpringSecurityConfig.java
+++ src/main/java/gyber/sapphire/config/SpringSecurityConfig.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -19,43 +22,82 @@ import gyber.sapphire.profile.UserCustomDetailsService;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfig  extends WebSecurityConfigurerAdapter{
+public class SpringSecurityConfig {
 
     @Autowired
     private UserCustomDetailsService userIPFSCustomDetailsService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userIPFSCustomDetailsService);
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+      
+            http
+            .authorizeHttpRequests(
+                (request) -> request
+                .requestMatchers("/home" , "/page")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+
+            ).formLogin((form) -> 
+             form.loginPage("/login")
+             .permitAll()
+             ).logout((logout) -> 
+             logout.permitAll());
+       
+
+         return http.build();
+
 
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+
+    @Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+
+    // @Override
+    // protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    //     auth.userDetailsService(userIPFSCustomDetailsService);
+
+    // }
+
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
     
-        http
-        .addFilterBefore(getKeyFilter(), UsernamePasswordAuthenticationFilter.class)
-        .addFilterBefore(getRefreshFilter() , UsernamePasswordAuthenticationFilter.class)
-        .addFilterBefore(getJwtFilter(), UsernamePasswordAuthenticationFilter.class)
-        .authorizeRequests()
-        .antMatchers("/pub/**")
-        .permitAll()
-        .antMatchers("/post/**")
-        .permitAll()
-        .antMatchers("/h2-console/**")
-        .permitAll()
-        .antMatchers("/auth/**")
-        .permitAll()
-        .antMatchers("/system/**")
-        .permitAll()
-        .anyRequest().authenticated()
-        .and()
-        .formLogin()
-        .and()
-        .logout()
-        .and()
-        .csrf().disable();
-    }
+    //     http
+    //     .addFilterBefore(getKeyFilter(), UsernamePasswordAuthenticationFilter.class)
+    //     .addFilterBefore(getRefreshFilter() , UsernamePasswordAuthenticationFilter.class)
+    //     .addFilterBefore(getJwtFilter(), UsernamePasswordAuthenticationFilter.class)
+    //     .authorizeRequests()
+    //     .antMatchers("/pub/**")
+    //     .permitAll()
+    //     .antMatchers("/post/**")
+    //     .permitAll()
+    //     .antMatchers("/h2-console/**")
+    //     .permitAll()
+    //     .antMatchers("/auth/**")
+    //     .permitAll()
+    //     .antMatchers("/system/**")
+    //     .permitAll()
+    //     .anyRequest().authenticated()
+    //     .and()
+    //     .formLogin()
+    //     .and()
+    //     .logout()
+    //     .and()
+    //     .csrf().disable();
+    // }
 
 
     @Bean
